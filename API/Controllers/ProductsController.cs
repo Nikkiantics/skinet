820139a8bf34799266227c81ebc8dec205998143
nikkiantics <nikkidilliner@gmail.com>
2022-12-28 14:35:02 -0500

    update redo 12-28-2022

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public IProductRepository Repo { get; }
+        public ProductsController(IProductRepository repo)
+        {
+            Repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+            {
+                var products = await Repo.GetProductsAsync();
+
+                return Ok(products);
+            }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+                return await Repo.GetProductByIdAsync(id);
+        }
+    }
+}
---
+++ Core/Entities/ProductType.cs
@@ -0,0 +1,9 @@
+namespace Core.Entities
+#nullable disable
+
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name {get; set;}
+    }
+}
