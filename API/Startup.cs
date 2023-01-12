f995366afd82d021fe329ab1b2b0a254aad4159a
nikkiantics <nikkidilliner@gmail.com>
2023-01-12 15:34:27 -0500

    01-12-2023 Update end of day

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,6 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
             {
