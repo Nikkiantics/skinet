e2a39b3022247145c5916e365b9fc3226d881084
nikkiantics <nikkidilliner@gmail.com>
2023-01-17 14:41:51 -0500

    01-17-2023 Update from section 9

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from '../core/nav-bar/nav-bar.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [NavBarComponent]
+})
+export class SharedModule { }
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent {
+products: IProduct[];
+
+constructor(private shopService: ShopService){
+
+}
+
+ngOnInit(){
+  this.shopService.getProducts().subscribe(response => {
+      this.products = response.data;
+  },
+
+  error => {
+  console.log(error);
+
+  });
+}
+}
